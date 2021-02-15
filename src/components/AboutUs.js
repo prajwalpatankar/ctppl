4e0016f424ed35c17c98af9f5392fc419acd7197
prajwalpatankar <patankarprajwal@gmail.com>
2021-02-15 23:19:04 +0530

    half data 2

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -65,11 +65,13 @@ class AboutUs extends Component {
                         <p>&nbsp;</p>
                     </div>
                     <div className="col-md-4 col-sm-12 vm-even">
+                        <img className="profile-photos" src="assets/img/team/img_avatar2.png" height="190px" width="190px" /> <br /><br />
                         <h2>Prakash Patankar</h2>
                         <h6>D I R E C T O R</h6><br /><br /><br />
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
                     <div className="col-md-4 col-sm-12 vm-even">
+                        <img className="profile-photos" src="assets/img/team/img_avatar2.png" height="190px" width="190px" /> <br /><br />
                         <h2>Aniket Patankar</h2>
                         <h6>D I R E C T O R</h6><br /><br /><br />
                         <p>
--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -51,10 +51,10 @@ class Footer extends Component {
                         
                     </div>
                     <div className="social-links text-center text-md-right pt-3 pt-md-0">
-                        <Link to="/" className="twitter"><i className="bx bxl-twitter"></i></Link>
-                        <Link to="/" className="facebook"><i className="bx bxl-facebook"></i></Link>
+                        {/* <Link to="/" className="twitter"><i className="bx bxl-twitter"></i></Link>
+                        <Link to="/" className="facebook"><i className="bx bxl-facebook"></i></Link> */}
                         <Link to="/" className="instagram"><i className="bx bxl-instagram"></i></Link>
-                        <Link to="/" className="google-plus"><i className="bx bxl-skype"></i></Link>
+                        {/* <Link to="/" className="google-plus"><i className="bx bxl-skype"></i></Link> */}
                         <Link to="/" className="linkedin"><i className="bx bxl-linkedin"></i></Link>
                     </div>
                 </div>
