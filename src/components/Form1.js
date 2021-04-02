9aabd3bb13a31b1196decb1e3b9f055e1b8a5c59
prajwalpatankar <patankarprajwal@gmail.com>
2021-04-02 20:40:58 +0530

    forms8

--- src/components/Form1.js
+++ src/components/Form1.js
@@ -2,32 +2,30 @@ import React from 'react';
 
 export default function Form() {
     return (
-        <div data-aos="fade-up">
-            <form name="contact_1" method="post" data-netlify="true" onSubmit="submit" className="contact-form row">
-                <input type="hidden" name="form-name" value="contact_1" /> <br />
-                <div  className="col-lg-6">
-                <label className="label" htmlFor="name">Name</label>
-                <input id="name" name="name" type="text" required />                    
+        <div>
+            <form name="contact_1" method="post" data-netlify="true" onSubmit="submit">
+                <input type="hidden" name="form-name" value="contact_1" />
+                <div>
+                <label>Name
+                <input id="name" name="name" type="text" required />     </label>               
                 </div>
-                <div  className="col-lg-6 ">
-                    <label className="label" htmlFor="email">E-mail</label>
-                    <input id="email" name="email" type="email" required />
+                <div>
+                    <label>E-mail
+                    <input id="email" name="email" type="email" required /></label>
                 </div>
-                <div  className="col-lg-6 ">
-                    <label className="label" htmlFor="phone">Contact Number</label>
-                    <input id="phone" name="phone" type="text" required />
+                <div>
+                    <label>Contact Number
+                    <input id="phone" name="phone" type="text" required /></label>
                 </div>
-                <div  className="col-lg-6 ">
-                    <label className="label" htmlFor="company">City</label>
-                    <input id="company" name="company" type="text" required />
+                <div>
+                    <label>City
+                    <input id="company" name="company" type="text" required /></label>
                 </div>
-                <div  className="col-lg-12">
-                    <label className="label" htmlFor="message">Particular</label>
-                    <input id="message" name="message" type="text" required />
+                <div>
+                    <label>Particular
+                    <input id="message" name="message" type="text" required /></label>
                 </div>
-                <div  className="col-lg-12">
-                    <button className="submit-btn" type="submit">Submit</button>
-                </div>
+                    <button type="submit">Submit</button>
             </form>
         </div>
     )
