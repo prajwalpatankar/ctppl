242227f0ad6d1b5ad4a3112942e960c6106e8255
prajwalpatankar <patankarprajwal@gmail.com>
2021-04-03 11:24:27 +0530

    hadnleSubmit added

--- src/components/Form1.js
+++ src/components/Form1.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
+
+
 export default function Form() {
+    const handleSubmit = e => {
+        fetch("/", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: {}
+        })
+          .then(() => alert("Success!"))
+          .catch(error => alert(error));
+  
+        e.preventDefault();
+      };
+
     return <div>
-            <form name="contact" method="post" data-netlify="true" onSubmit="submit" data-netlify-honeypot="bot-field">
+            <form name="contact" method="post" data-netlify="true" onSubmit={handleSubmit} data-netlify-honeypot="bot-field">
                 <input type="hidden" name="form-name" value="contact" /><br /><br /><br /><br /><br /><br /><br /><br /><br />
                 <p>
                 <label>Name
