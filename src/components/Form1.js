948fc73b19b91e77280d9fa7ff43db266865122c
prajwalpatankar <patankarprajwal@gmail.com>
2021-04-03 11:11:11 +0530

    working form

--- src/components/Form1.js
+++ src/components/Form1.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Form() {
     return <div>
-            <form name="contact" method="post" netlify onSubmit="submit" netlify-honeypot="bot-field">
+            <form name="contact" method="post" data-netlify="true" onSubmit="submit" data-netlify-honeypot="bot-field">
                 <input type="hidden" name="form-name" value="contact" /><br /><br /><br /><br /><br /><br /><br /><br /><br />
                 <p>
                 <label>Name
