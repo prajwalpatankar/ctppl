e1ff95181a1d06415a01c1b1d4fddc2bba567a8a
prajwalpatankar <patankarprajwal@gmail.com>
2024-11-08 17:07:58 -0800

    revert

--- src/components/OnGoing.js
+++ src/components/OnGoing.js
@@ -10,112 +10,91 @@ class OnGoing extends Component {
         this.state = {
             industrial: [
                 {
-                    id: 45,
-                    name: " CG Power And Industrial Solutions Ltd, Bhopal",
-                    type: 1,
-                    desc: "Industrial Project | Area - 85,000 sqft | Cost - 16 Cr | Civil, Foundations, Road | Consultant - Shaha & Talathi, Vadodara | Location - Mandideep, Bhopal",
-                    img: "assets/img/projects/cg_power.png"
-                },
-                {
-                    id: 46,
-                    name: "Kirloskar Pneumatic Company Limited, Nashik",
-                    type: 1,
-                    desc: "Industrial Project | Area - 50,000 sqft | Cost - 12.5 Cr | Civil, PEB, Foundations, Design Build | Consultant - Patankar's Creative Turnkey Projects Pvt Ltd, Pune | Location - Nashik",
-                    img: "assets/img/projects/kirloskar_pn.png"
-                },
-                {
                     id: 3,
                     name: "Garware Bestretch, Wai",
-                    type: 2,
-                    desc: "Industrial Sheds| Cost - 900 Lacs ",
-                    img: "assets/img/projects/garware-bestrec.jpg"
+                    type: 2, 
+                    desc : "Industrial Sheds| Cost - 900 Lacs ",
+                    img: "assets/img/projects/garware-bestrec.jpg" 
                 },
                 {
                     id: 3,
                     name: "VTP Pegasus",
-                    type: 2,
-                    desc: "Scope - Podium Core & Shell works | Cost - 540 Lacs | Location - Manjri, Pune",
-                    img: "assets/img/projects/VTP Pegasus.jpeg"
+                    type: 2, 
+                    desc : "Scope - Podium Core & Shell works | Cost - 540 Lacs | Location - Manjri, Pune",
+                    img: "assets/img/projects/VTP Pegasus.jpeg" 
                 },
                 {
                     id: 3,
                     name: "Exide Industries Ltd.",
-                    type: 2,
-                    desc: "Scope - Extension of Submarine Shed | Cost - 400 Lacs | Location- Taloja, Pune",
-                    img: "assets/img/projects/garware-bestrec.jpg"
+                    type: 2, 
+                    desc : "Scope - Extension of Submarine Shed | Cost - 400 Lacs | Location- Taloja, Pune",
+                    img: "assets/img/projects/garware-bestrec.jpg" 
                 },
             ],
             residential: [
                 {
-                    id: 50,
-                    name: "Joyville Manjri Hadapsar Annex, Pune",
-                    type: 2,
-                    desc: "Finishing | Area - 1,80,000 sqft | Cost - 7.8 Cr | Shevalwadi, Manjari",
-                    img: "assets/img/projects/joyville.png"
-                },
-                {
                     id: 2,
                     name: "Godrej Elements, Pune",
-                    type: 2,
-                    desc: "Residential projects | cost- 1021 Lacs | consultant- Kapadia Associates, | Location- Hinjewadi Pune",
+                    type: 2, 
+                    desc : "Residential projects | cost- 1021 Lacs | consultant- Kapadia Associates, | Location- Hinjewadi Pune",
                     img: "assets/img/projects/elements.jpg"
                 },
                 {
                     id: 5,
                     name: "Godrej Park Greens, Pune",
-                    type: 2,
-                    desc: "Residential Project | Finishing Work | Cost: 850 Lacs | Client - Godrej Properties Limited | Location: Mamurdi, Pune",
-                    img: "assets/img/projects/Godrej Greens.jpg"
+                    type: 2, 
+                    desc : "Residential Project | Finishing Work | Cost: 850 Lacs | Client - Godrej Properties Limited | Location: Mamurdi, Pune",
+                    img: "assets/img/projects/Godrej Greens.jpg" 
                 },
                 {
                     id: 5,
                     name: "Godrej Nurture",
-                    type: 2,
-                    desc: "Residential Project | Scope -  Tower finishing works | Cost- 720 Lacs | Client - Godrej Properties Limited | Location- Mamurdi, Pune.",
-                    img: "assets/img/projects/godrej nurture.jpeg"
+                    type: 2, 
+                    desc : "Residential Project | Scope -  Tower finishing works | Cost- 720 Lacs | Client - Godrej Properties Limited | Location- Mamurdi, Pune.",
+                    img: "assets/img/projects/godrej nurture.jpeg" 
                 },
                 {
                     id: 5,
                     name: "Avante Spaces Ltd by Kirloskar",
-                    type: 2,
-                    desc: "Residential Project | Scope -  Landscape, Hardscape & Softscape development for Commercial Building | Cost - 300 Lacs | Client - Godrej Properties Limited | Location- Kothrud, Pune",
-                    img: ""
+                    type: 2, 
+                    desc : "Residential Project | Scope -  Landscape, Hardscape & Softscape development for Commercial Building | Cost - 300 Lacs | Client - Godrej Properties Limited | Location- Kothrud, Pune",
+                    img: "" 
                 },
                 {
                     id: 5,
                     name: "Isprava Vesta Pvt. Ltd.",
-                    type: 2,
-                    desc: "Residential Project | Scope - Core & Shell works for Luxury Bungalows & Infra works | Cost - 740 Lacs | Client - Godrej Properties Limited | Location- Moira, Goa.",
-                    img: ""
+                    type: 2, 
+                    desc : "Residential Project | Scope - Core & Shell works for Luxury Bungalows & Infra works | Cost - 740 Lacs | Client - Godrej Properties Limited | Location- Moira, Goa.",
+                    img: "" 
                 },
             ],
             itbuildings: [
                 {
                     id: 3,
                     name: "Solitaire Business Hub",
-                    type: 2,
-                    desc: "Scope - Finishing & Infra works| Cost - 640 Lacs | Location - Baner, Pune",
-                    img: ""
+                    type: 2, 
+                    desc : "Scope - Finishing & Infra works| Cost - 640 Lacs | Location - Baner, Pune",
+                    img: "" 
                 },
             ],
             mep: [
-
+                
             ]
         }
     }
 
     render() {
         let industrial = this.state.industrial.map(person => {
-            if (person.img !== "") {
+            if(person.img !== "") {
                 return (
-                    <Col lg="4" md="12" sm="12" data-aos="fade-up">
+                    <Col lg="4" md= "12" sm= "12" data-aos="fade-up">
                         <ProjectCard key={person.id} person={person} />
                     </Col>
                 )
             }
             else {
                 return (
-                    <Col lg="4" md="12" sm="12" data-aos="fade-up">
+                    <Col lg="4" md= "12" sm= "12" data-aos="fade-up">
                         <NoImageCard key={person.id} person={person} />
                     </Col>
                 )
@@ -138,32 +117,32 @@ class OnGoing extends Component {
         //     }
         // })
         let residential = this.state.residential.map(person => {
-            if (person.img !== "") {
+            if(person.img !== "") {
                 return (
-                    <Col lg="4" md="12" sm="12" data-aos="fade-up">
+                    <Col lg="4" md= "12" sm= "12" data-aos="fade-up">
                         <ProjectCard key={person.id} person={person} />
                     </Col>
                 )
             }
             else {
                 return (
-                    <Col lg="4" md="12" sm="12" data-aos="fade-up">
+                    <Col lg="4" md= "12" sm= "12" data-aos="fade-up">
                         <NoImageCard key={person.id} person={person} />
                     </Col>
                 )
             }
         })
         let itbuildings = this.state.itbuildings.map(person => {
-            if (person.img !== "") {
+            if(person.img !== "") {
                 return (
-                    <Col lg="4" md="12" sm="12" data-aos="fade-up">
+                    <Col lg="4" md= "12" sm= "12" data-aos="fade-up">
                         <ProjectCard key={person.id} person={person} />
                     </Col>
                 )
             }
             else {
                 return (
-                    <Col lg="4" md="12" sm="12" data-aos="fade-up">
+                    <Col lg="4" md= "12" sm= "12" data-aos="fade-up">
                         <NoImageCard key={person.id} person={person} />
                     </Col>
                 )
@@ -190,8 +169,8 @@ class OnGoing extends Component {
                 <br />
                 <h1><strong data-aos="fade-up">Industrial</strong></h1>
                 <Container fluid data-aos="fade-up" data-aos-delay="200">
-                    <Row>
-                        {industrial}
+                    <Row>                        
+                        {industrial}                        
                     </Row>
                 </Container>
                 {/* <br /><hr /><br />                                      uncomment in case of new institutional project
@@ -204,15 +183,15 @@ class OnGoing extends Component {
                 <br /><hr /><br />
                 <h1><strong data-aos="fade-up">Residential</strong></h1>
                 <Container fluid data-aos="fade-up" data-aos-delay="200">
-                    <Row>
-                        {residential}
+                    <Row>                        
+                        {residential}                        
                     </Row>
                 </Container>
                 <br /><hr /><br />
                 <h1><strong data-aos="fade-up">IT Buildings</strong></h1>
                 <Container fluid data-aos="fade-up" data-aos-delay="200">
-                    <Row>
-                        {itbuildings}
+                    <Row>                        
+                        {itbuildings}                        
                     </Row>
                 </Container>
                 <br /><hr /><br />
@@ -224,7 +203,7 @@ class OnGoing extends Component {
                 </Container> */}
                 <br /><br />
             </div>
-
+            
         )
     }
 }
